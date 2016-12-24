30e3ec04dcc38a9bf1f686424970ec66d68da0ae
chakri <chakchelsea@gmail.com>
2016-12-24 18:31:07 +0530

    PHP Sample Code

---
+++ src/dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+$host = " mysqltestdb.cagxpatwqnxk.us-west-2.rds.amazonaws.com";
+$user = "root";
+$password = "Secure*!2";
+$datbase = "employee";
+mysql_connect($host,$user,$password);
+mysql_select_db($datbase);
+?>
